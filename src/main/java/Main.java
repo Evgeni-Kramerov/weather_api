2636b3f6113d0f3f4ab6ac88a591ab0adb4080a5
Evgeni Kramerov <ekarmerov@gmail.com>
2025-02-08 20:55:14 +0200

    api commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,22 @@ import utils.EnvVariables;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         WeatherAPI weatherAPI = new WeatherAPI();
         City city = weatherAPI.getCity("London");
+
+
+
+        long unixTimestamp = Instant.now().getEpochSecond();
+
+        String date = Long.toString(unixTimestamp);
+
+        System.out.println(date);
         
     }
 }
