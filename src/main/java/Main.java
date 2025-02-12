4b0575acfbed80d4d10ab4862129293014fedd96
Evgeni Kramerov <ekarmerov@gmail.com>
2025-02-12 21:18:44 +0200

    Added API Request for Historical Data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,3 @@
-import api.WeatherAPI;
-import model.City;
-
-import java.io.IOException;
-import java.time.Instant;
-
-/*TODO 2) Get last 30 days from API
-       3) Object Data presentation
-
-
-*/
 public class Main {
-    public static void main(String[] args) throws IOException {
-        WeatherAPI weatherAPI = new WeatherAPI();
-        City city = weatherAPI.getCity("Haifa");
-
-
-        System.out.println(city);
-
-        long unixTimestamp = Instant.now().getEpochSecond();
-
-        String date = Long.toString(unixTimestamp);
-
-        System.out.println(date);
-        
-    }
+    public static void main(String[] args) {}
 }
